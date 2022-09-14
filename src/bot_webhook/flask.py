ad1d8b6db266dec7528bddbd44dac205da46d660
Gedoy <773673499@qq.com>
2022-09-14 16:51:36 +0800

    fix

--- src/bot_webhook/flask.py
+++ src/bot_webhook/flask.py
@@ -5,17 +5,19 @@ app = Flask(__name__)
 
 @app.route('/webhook/github', methods=['POST'])
 def github():
-    data = request.get_json()
-    print(data)
-    bot = Bot()
-    bot.send({
-    "sessionKey": bot.session,
-        "target":946063184,
-        "messageChain":[
-            { "type":"Plain", "text":f"GitHub Push at {data.get('repository').get('name')}\n" },
-            { "type":"Plain", "text":f"ref: {data.get('ref')}\n" },
-            { "type":"Plain", "text":f"commit: {data.get('head_commit').get('id')[-8:]}\n" },
-            { "type":"Plain", "text":f"message: {data.get('head_commit').get('message')}\n" },
-            { "type":"Plain", "text":f"author: {data.get('head_commit').get('author').get('name')}\n" },
-        ]
-    }, 'sendGroupMessage')
+    event = request.headers.get('X-GitHub-Event')
+    if event == 'push':
+        data = request.get_json()
+        print(data)
+        bot = Bot()
+        bot.send({
+        "sessionKey": bot.session,
+            "target":946063184,
+            "messageChain":[
+                { "type":"Plain", "text":f"GitHub Push at {data.get('repository').get('name')}\n" },
+                { "type":"Plain", "text":f"ref: {data.get('ref')}\n" },
+                { "type":"Plain", "text":f"commit: {data.get('head_commit').get('id')[-8:]}\n" },
+                { "type":"Plain", "text":f"message: {data.get('head_commit').get('message')}\n" },
+                { "type":"Plain", "text":f"author: {data.get('head_commit').get('author').get('name')}\n" },
+            ]
+        }, 'sendGroupMessage')
