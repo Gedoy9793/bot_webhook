1c701948e6b8a3bde4dd0cc1702518da8b9a0a2f
Gedoy <773673499@qq.com>
2022-09-14 16:39:15 +0800

    fix

--- src/bot_webhook/flask.py
+++ src/bot_webhook/flask.py
@@ -13,7 +13,7 @@ def github():
         "messageChain":[
             { "type":"Plain", "text":f"GitHub Push at {data.get('repository').get('name')}\n" },
             { "type":"Plain", "text":f"ref: {data.get('ref')}\n" },
-            { "type":"Plain", "text":f"commit: {data.get('after')[-8:]}\n" },
+            { "type":"Plain", "text":f"commit: {data.get('head_commit').get('id')[-8:]}\n" },
             { "type":"Plain", "text":f"message: {data.get('head_commit').get('message')}\n" },
             { "type":"Plain", "text":f"author: {data.get('head_commit').get('author').get('name')}\n" },
         ]
