43ec1ad733773400dd1901dd726231ae055f7fde
Gedoy <773673499@qq.com>
2022-09-14 16:22:39 +0800

    更新gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -4,8 +4,6 @@ bind = '0.0.0.0:18002'
 workers = multiprocessing.cpu_count() * 2 + 1
 
 backlog = 2048
-worker_class = "gevent"
-worker_connections = 1000
 daemon = False
 debug = True
 proc_name = 'gunicorn_demo'
