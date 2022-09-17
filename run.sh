bbbcb043618e422e8a997f03c6e3a7979057bea8
“CreatingLiu <773673499@qq.com>
2022-09-17 21:37:22 +0800

    更新脚本

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 pdm install
 git pull
 cat log/gunicorn.pid | awk '{print "kill "$1}' | sh
-.venv/bin/gunicorn -c gunicorn.py bot_webhook.wsgi:application
+exec .venv/bin/gunicorn -c gunicorn.py bot_webhook.wsgi:application
